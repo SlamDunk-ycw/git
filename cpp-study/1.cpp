5e8e62cec27a3e6ded90d3b952d239464e7ed290
SlamDunk-ycw <1363472590@qq.com>
2021-08-14 09:18:25 +0800

    cpp-study

---
+++ cpp-study/1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    float a=0.00000678f;
+    double b=0.00000678;
+    a=a+111111.111f;
+    b=b+111111.111;
+
+    cout<<"a="<<a<<"b="<<b<<endl;
+    return 0;
+}
