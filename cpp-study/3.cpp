ef653353e5e6f72f8d3c7ed2bedfe61af10fb3a3
SlamDunk-ycw <1363472590@qq.com>
2021-08-14 10:33:49 +0800

    函数的默认参数和函数重载

---
+++ cpp-study/3.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+/*默认参数和函数的重载*/
+
+//函数在定义时可以设定默认值，这样在调用时如果形参和实参数目不一致，这采用默认值
+//默认值的参数要放后面
+
+/*函数重载*/
+//同一个域中，同一个函数名，来定义多个函数，但是函数的参数列表应该彼此不同，或者是参数个数不同，
+int add(int a,int b,int c=2)
+{
+    return a+b+c;
+}
+double add(double a,double b)//函数重载
+{
+    return a+b;
+}
+int main()
+{
+    int a=1,b=1;
+    cout<<add(1,2)<<" "<<add(1,2,3)<<endl;
+    cout<<add(1.0,2.1);
+    return 0;
+}
