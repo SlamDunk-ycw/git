819d909aaac6b708453bec487911b063a45999d4
SlamDunk-ycw <1363472590@qq.com>
2021-08-25 22:58:10 +0800

    STL

---
+++ cpp-class/STL.cpp
@@ -0,0 +1,16 @@
+#include<sstream>
+#include<cctype>
+#include<vector>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	/*string a("adjfh	");
+	bool b=iscntrl(a[5]);
+	cout << b;
+	return 0;*/
+	vector<int> a;
+	a.push_back(1);
+	a.reserve(10);
+}
